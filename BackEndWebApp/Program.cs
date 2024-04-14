d40cb353e754d50aacaeae1cca1f3afe822953c1
GLRandula <lakithrandula28@gmail.com>
2024-04-14 21:02:40 +0530

    Update the code.

--- BackEndWebApp/Program.cs
+++ BackEndWebApp/Program.cs
@@ -7,6 +7,13 @@ var app = builder.Build();
 
 app.UseRewriter(new RewriteOptions().AddRedirect("tasks/(.*)", "todos/$1"));
 
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"[{context.Request.Method} {context.Request.Path} {DateTime.UtcNow}] Started.");
+    await next(context);
+    Console.WriteLine($"[{context.Request.Method} {context.Request.Path} {DateTime.UtcNow}] Finished.");
+});
+
 var todos = new List<Todo>();
 
 app.MapGet("/todos", ()  => todos);
